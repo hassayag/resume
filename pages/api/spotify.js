e94d3877a80b5c8e0307dc2b27b055f4d10f57d2
Harry <hazzarius@gmail.com>
2023-09-23 13:22:19 +0100

    embed spotify

---
+++ pages/api/spotify.js
@@ -0,0 +1,113 @@
+import queryString from 'querystring';
+import crypto from 'crypto'
+
+const CLIENT_ID = process.env.SPOTIFY_CLIENT_ID;
+
+let tokenExpiresAt = Date.now();
+let lastToken = '';
+
+export default async function handler(req, res) {
+    // const endpoint = 'https://api.spotify.com/v1/me/player/recently-played'
+    const endpoint = 'https://api.spotify.com/v1/playlists/6OxOl1VenOr3gY4E0kDw7q/tracks'
+    const token = await getToken();
+    const options = {
+        method: 'GET',
+        headers: {
+          'Authorization': `Bearer ${token}`,
+          'Content-Type': 'application/json',
+        },
+      };
+
+    const response = await fetch(endpoint, options);
+    if (response.status !== 200) {
+        res.status(response.status).send(response.statusText)
+        return;
+    }
+
+    const data = await response.json();
+
+    const trackUris= data.items?.map(item => item.track.id)
+
+    const randomUri = trackUris[Math.floor(trackUris.length * Math.random())]
+
+    // const iframe = await getIFrame(randomUri)
+    // if (!iframe) {
+    //     res.status(404).send('not found')
+    // }
+    res.status(200).send(randomUri);
+}  
+
+async function getIFrame(uri) {
+    const token = await getToken();
+    const options = {
+        method: 'GET',
+        headers: {
+          'Authorization': `Bearer ${token}`,
+          'Content-Type': 'application/json',
+        },
+      };
+
+    // const response = await fetch(`https://open.spotify.com/embed-track/iframe-api/v1/${uri}`, options);
+    const response = await fetch(`https://open.spotify.com/embed-podcast/iframe-api/v1/spotify:episode:00ANHlyEzJBowXkrljDvzr`, options);
+
+    console.log({response})
+    if (response.status !== 200) {
+        return;
+    }
+
+    const iframe = await response.text()
+    return iframe
+}
+
+async function getToken() {
+    const endpoint = 'https://accounts.spotify.com/api/token'
+
+    const requestBody = {
+        grant_type: 'client_credentials',
+        client_id: process.env.SPOTIFY_CLIENT_ID,
+        client_secret: process.env.SPOTIFY_CLIENT_SECRET,
+    };
+
+    const options = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: queryString.stringify(requestBody),
+    }
+
+    if (!lastToken || Date.now() > tokenExpiresAt) {
+        const response = await fetch(endpoint, options)
+        const data = await response.json();
+
+        lastToken = data.access_token;
+        tokenExpiresAt = Date.now() + data.expires_in * 1000
+        return data.access_token;
+    }
+
+    return lastToken;
+}
+
+async function login() {
+    var redirect_uri = 'http://localhost:3002/';    
+    var state = crypto.randomBytes(20).toString('hex');
+    var scope = 'user-read-private user-read-email';
+
+    const requestBody = {
+        response_type: 'code',
+        client_id: CLIENT_ID,
+        scope: scope,
+        redirect_uri: redirect_uri,
+        state: state
+    };
+
+    const response = await fetch('https://accounts.spotify.com/authorize?' + queryString.stringify(requestBody))
+    if (response.status !== 200) {
+        res.status(response.status).send(response.statusText)
+        return;
+    }
+    
+    const data = await response.text()
+    console.log(data)
+    return data;
+}
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,30 @@
-import Head from 'next/head';
-import commonStyles from './common.module.sass';
-import styles from './home.module.sass';
+import React, { useEffect } from "react";
+import Head from "next/head";
+import commonStyles from "./common.module.sass";
+import styles from "./home.module.sass";
 
 export default function Home() {
+  const embedSpotify = async () => {
+    const response = await fetch("/api/spotify");
+    const uri = await response.text();
+    const element = document.getElementById("embed-iframe");
+
+    element.innerHTML = `
+      <iframe
+        style="border-radius:12px"
+        src="https://open.spotify.com/embed/track/${uri}?utm_source=generator"
+        width="20%"
+        height="10%"
+        frameBorder="0"
+        allowfullscreen=""
+        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+        loading="lazy"
+      ></iframe>
+    `
+  };
+
+    useEffect(() => embedSpotify())
+
   return (
     <div className={commonStyles.container}>
       <Head>
@@ -11,36 +33,40 @@ export default function Home() {
       </Head>
 
       <main>
-        <a id='home'/> 
+        <a id="home" />
         <h1 className={styles.title}>
           Harry <a>Assayag</a>
           <div className={styles.links}>
-          <a
-            href="https://github.com/hassayag"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img src="/github.svg" alt="github" className={styles.link}/>
-          </a>
-          <a
-            href="https://uk.linkedin.com/in/harryassayag"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img src="/linkedin.svg" alt="linkedin" className={styles.link}/>
-          </a>
+            <a
+              href="https://github.com/hassayag"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src="/github.svg" alt="github" className={styles.link} />
+            </a>
+            <a
+              href="https://uk.linkedin.com/in/harryassayag"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                src="/linkedin-blue.svg"
+                alt="linkedin"
+                className={styles.link}
+              />
+            </a>
           </div>
         </h1>
-
+        <div id="embed-iframe"></div>
         <div className={styles.nav}>
-        <a href="#home" className={styles.card}>
+          <a href="#home" className={styles.card}>
             Home
           </a>
 
           <a href="#about" className={styles.card}>
             About
           </a>
-          
+
           <a href="#portfolio" className={styles.card}>
             Portfolio
           </a>
@@ -53,43 +79,51 @@ export default function Home() {
             Contact
           </a>
         </div>
-
         <div className={commonStyles.sectionBorder}>
-          <a id='about'/>
+          <a id="about" />
           <div className={commonStyles.section}>
             <h2 className={commonStyles.heading}>About me</h2>
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Error minima cum quo adipisci iste, aliquam odio. Impedit quidem repudiandae officiis repellat omnis, molestiae, et aliquid nihil ea officia non aut?
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
+              minima cum quo adipisci iste, aliquam odio. Impedit quidem
+              repudiandae officiis repellat omnis, molestiae, et aliquid nihil
+              ea officia non aut?
             </p>
           </div>
         </div>
-
         <div className={commonStyles.sectionBorder}>
-          <a id='portfolio'/>
+          <a id="portfolio" />
           <div className={commonStyles.section}>
             <h2 className={commonStyles.heading}>Portfolio</h2>
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Error minima cum quo adipisci iste, aliquam odio. Impedit quidem repudiandae officiis repellat omnis, molestiae, et aliquid nihil ea officia non aut?
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
+              minima cum quo adipisci iste, aliquam odio. Impedit quidem
+              repudiandae officiis repellat omnis, molestiae, et aliquid nihil
+              ea officia non aut?
             </p>
           </div>
         </div>
-
         <div className={commonStyles.sectionBorder}>
-          <a id='gear'/>
+          <a id="gear" />
           <div className={commonStyles.section}>
             <h2 className={commonStyles.heading}>Gear</h2>
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Error minima cum quo adipisci iste, aliquam odio. Impedit quidem repudiandae officiis repellat omnis, molestiae, et aliquid nihil ea officia non aut?
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
+              minima cum quo adipisci iste, aliquam odio. Impedit quidem
+              repudiandae officiis repellat omnis, molestiae, et aliquid nihil
+              ea officia non aut?
             </p>
           </div>
         </div>
-
         <div className={commonStyles.sectionBorder}>
-          <a id='contact'/>
+          <a id="contact" />
           <div className={commonStyles.section}>
             <h2 className={commonStyles.heading}>Contact</h2>
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Error minima cum quo adipisci iste, aliquam odio. Impedit quidem repudiandae officiis repellat omnis, molestiae, et aliquid nihil ea officia non aut?
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
+              minima cum quo adipisci iste, aliquam odio. Impedit quidem
+              repudiandae officiis repellat omnis, molestiae, et aliquid nihil
+              ea officia non aut?
             </p>
           </div>
         </div>
@@ -101,59 +135,10 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
         </a>
       </footer>
-
-      <style jsx>{`
-        main {
-          padding: 0;
-        }
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family:
-            Menlo,
-            Monaco,
-            Lucida Console,
-            Liberation Mono,
-            DejaVu Sans Mono,
-            Bitstream Vera Sans Mono,
-            Courier New,
-            monospace;
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          min-height: 100%;
-          padding: 0;
-          margin: 0;
-          background-color: rgb(230, 230, 230);
-          font-family:
-            -apple-system,
-            BlinkMacSystemFont,
-            Segoe UI,
-            Roboto,
-            Oxygen,
-            Ubuntu,
-            Cantarell,
-            Fira Sans,
-            Droid Sans,
-            Helvetica Neue,
-            sans-serif;
-        }
-        html {
-          scroll-behavior: smooth;
-        }
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
     </div>
   );
 }
